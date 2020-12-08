f42161579854dd3f4b6c79339726fd296e473f90
ketchyang <ketchyang@outlook.com>
2020-12-08 20:54:26 +0800

    sub category

--- client/src/views/CategoryList.vue
+++ client/src/views/CategoryList.vue
@@ -1,66 +1,62 @@
 <template>
-    <div>
-        <h1>分类列表</h1>
-        <el-table :data="items">
-            <el-table-column
-                prop="_id"
-                label="ID"
-                width="220"
-            ></el-table-column>
-            <el-table-column prop="name" label="分类名称"></el-table-column>
-            <el-table-column label="操作">
-                <template slot-scope="scope">
-                    <el-button
-                        size="mini"
-                        @click="handleEdit(scope.$index, scope.row)"
-                        >编辑</el-button
-                    >
-                    <el-button
-                        size="mini"
-                        type="danger"
-                        @click="handleDelete(scope.$index, scope.row)"
-                        >删除</el-button
-                    >
-                </template>
-            </el-table-column>
-        </el-table>
-    </div>
+  <div>
+    <h1>分类列表</h1>
+    <el-table :data="items">
+      <el-table-column prop="_id" label="ID" width="220"></el-table-column>
+      <el-table-column prop="name" label="分类名称"></el-table-column>
+      <el-table-column prop="parent.name" label="上级分类"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">
+            编辑
+          </el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+          >
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            items: [],
-        };
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    async fetch() {
+      const res = await this.$http.get('categories');
+      this.items = res.data;
     },
-    methods: {
-        async fetch() {
-            const res = await this.$http.get('categories');
-            this.items = res.data;
-        },
-        handleEdit(index, row) {
-            this.$router.push(`/categories/edit/${row._id}`);
-        },
-        async handleDelete(index, row) {
-            this.$confirm(`是否确认要删除分类"${row.name}"?`, '提示', {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'warning',
-            }).then(async () => {
-                const res = await this.$http.delete(`categories/${row._id}`);
-                if (res.statusText === 'OK') {
-                    this.$message({
-                        type: 'success',
-                        message: '删除成功!',
-                    });
-                    this.fetch();
-                }
-            });
-        },
+    handleEdit(index, row) {
+      this.$router.push(`/categories/edit/${row._id}`);
     },
-    created() {
-        this.fetch();
+    async handleDelete(index, row) {
+      this.$confirm(`是否确认要删除分类"${row.name}"?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }).then(async () => {
+        const res = await this.$http.delete(`categories/${row._id}`);
+        if (res.statusText === 'OK') {
+          this.$message({
+            type: 'success',
+            message: '删除成功!',
+          });
+          this.fetch();
+        }
+      });
     },
+  },
+  created() {
+    this.fetch();
+  },
 };
 </script>

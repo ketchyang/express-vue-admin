da98ef8a642de2d4f1cd0ead68afbd1b11c369e2
ketchyang <ketchyang@outlook.com>
2020-12-08 20:34:52 +0800

    sub category

--- client/src/views/CategoryEdit.vue
+++ client/src/views/CategoryEdit.vue
@@ -3,7 +3,17 @@
         <h1>{{ id ? '编辑' : '新建' }}分类</h1>
         <el-form @submit.native.prevent="save">
             <el-form-item label="名称">
-                <el-input v-model="model.name"></el-input>
+                <el-input v-model="model.name"> </el-input>
+            </el-form-item>
+            <el-form-item label="上级分类">
+                <el-select v-model="model.parent">
+                    <el-option
+                        v-for="item in parents"
+                        :key="item._id"
+                        :label="item.name"
+                        :value="item._id"
+                    ></el-option>
+                </el-select>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" native-type="submit">保存</el-button>
@@ -20,6 +30,7 @@ export default {
     data() {
         return {
             model: {},
+            parents: [],
         };
     },
     methods: {
@@ -42,9 +53,14 @@ export default {
             const res = await this.$http.get(`categories/${this.id}`);
             this.model = res.data;
         },
+        async fetchParents() {
+            const res = await this.$http.get('categories');
+            this.parents = res.data;
+        },
     },
     created() {
         this.id && this.fetch();
+        this.fetchParents();
     },
 };
 </script>
